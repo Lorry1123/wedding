fa7e66754c58aa7e91679b13b8257b1c8e5f906b
Lorry <Lorry1123@outlook.com>
2019-09-14 22:30:45 +0800

    MOD: bless page input area & userinfo

--- pages/bless/bless.js
+++ pages/bless/bless.js
@@ -1,18 +1,63 @@
 // pages/bless/bless.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    message: '',
   },
-
+  onMessageChange: function(data, event) {
+    this.setData({
+      message: data.detail.value,
+    })
+  },
+  sendMessage: function() {
+    console.log('data: ', this.data.message);
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        console.log(res.userInfo);
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        },
+        fail: res => {
+          console.log('fail: ', res)
+        }
+      })
+    }
+  },
+  getUserInfo: function (e) {
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
   },
 
   /**
