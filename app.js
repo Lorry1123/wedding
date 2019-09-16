207e00d5d5d171a39743919df6fadd0eb12b983f
Lorry <Lorry1123@outlook.com>
2019-09-16 21:59:06 +0800

    MOD: 邀请函样式优化

--- app.js
+++ app.js
@@ -1,53 +1,53 @@
 //app.js
 App({
-  // onLaunch: function() {
-  //   // 展示本地存储能力
-  //   var logs = wx.getStorageSync('logs') || []
-  //   logs.unshift(Date.now())
-  //   wx.setStorageSync('logs', logs)
+  onLaunch: function() {
+    // // 展示本地存储能力
+    // var logs = wx.getStorageSync('logs') || []
+    // logs.unshift(Date.now())
+    // wx.setStorageSync('logs', logs)
 
-  //   // 登录
-  //   wx.login({
-  //     success: res => {
-  //       // 发送 res.code 到后台换取 openId, sessionKey, unionId
-  //     }
-  //   })
-  //   // 获取用户信息
-  //   wx.getSetting({
-  //     success: res => {
-  //       if (res.authSetting['scope.userInfo']) {
-  //         // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-  //         wx.getUserInfo({
-  //           success: res => {
-  //             // 可以将 res 发送给后台解码出 unionId
-  //             this.globalData.userInfo = res.userInfo
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
 
-  //             // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-  //             // 所以此处加入 callback 以防止这种情况
-  //             if (this.userInfoReadyCallback) {
-  //               this.userInfoReadyCallback(res)
-  //             }
-  //           }
-  //         })
-  //       }
-  //     }
-  //   })
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
 
-  //   var innerAudioContext = wx.createInnerAudioContext();
-  //   innerAudioContext.autoplay = false;
-  //   innerAudioContext.src = 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46';
-  //   innerAudioContext.loop = true;
+    // var innerAudioContext = wx.createInnerAudioContext();
+    // innerAudioContext.autoplay = false;
+    // innerAudioContext.src = 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46';
+    // innerAudioContext.loop = true;
 
-  //   console.log('???');
-  //   innerAudioContext.onPlay(() => {
-  //     console.log('开始播放');
-  //   })
-  //   innerAudioContext.onError((res) => {
-  //     console.log(res.errMsg);
-  //     console.log(res.errCode);
-  //   })
-  //   console.log(innerAudioContext);
-  // },
+    // console.log('???');
+    // innerAudioContext.onPlay(() => {
+    //   console.log('开始播放');
+    // })
+    // innerAudioContext.onError((res) => {
+    //   console.log(res.errMsg);
+    //   console.log(res.errCode);
+    // })
+    // console.log(innerAudioContext);
+  },
   globalData: {
     userInfo: null
   }
